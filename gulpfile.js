d7fc23466a33644c15142cb4df27dad824450aa2
Craig Morrison <hello@craigmorrison.co.uk>
2018-08-19 16:25:34 +0100

    Updgrade to Gulp v4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,23 @@ const paths = {
   css: {
     entry: './src/*.css',
     watch: './src/**/*.css',
-    dist: './dist'
-  }
+    dist: './dist',
+  },
 };
 
-gulp.task('styles', () => {
+function styles() {
   return gulp
     .src(paths.css.entry)
     .pipe(postcss({ modules: true }))
     .pipe(gulp.dest(paths.css.dist));
-});
+}
 
-gulp.task('watch', () => {
-  gulp.watch(paths.css.watch, ['styles']);
-});
+function watch() {
+  return gulp.watch(paths.css.watch, styles);
+}
 
 // Dev
-gulp.task('dev', ['styles', 'watch']);
+gulp.task('dev', gulp.series(styles, watch));
 
-// Build
-gulp.task('default', ['styles']);
+// Build (Default)
+gulp.task('default', gulp.series(styles));
